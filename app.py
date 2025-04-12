2ab8471690d081ae0c62a0aaae6fc5ff4e549556
eng8890 <winer.net55@gmail.com>
2025-04-12 19:42:07 +0300

    app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def mono_encrypt(text, key_map):
 def index():
     encrypted_text = ""
     plaintext = ""
+    keymap = ""
     if request.method == 'POST':
         plaintext = request.form.get('plaintext', '')
         keymap = request.form.get('keymap', '')
@@ -47,11 +48,17 @@ def index():
             encrypted_text = mono_encrypt(plaintext, key_dict)
         except:
             encrypted_text = "Error in key format"
-        return render_template('index.html', encrypted=encrypted_text, keymap=keymap, plaintext=plaintext)
     else:
         key_dict = generate_key()
         keymap = key_to_string(key_dict)
-        return render_template('index.html', encrypted="", keymap=keymap, plaintext="")
+
+    return render_template('index.html', encrypted=encrypted_text, keymap=keymap, plaintext=plaintext)
+
+@app.route('/generate-key', methods=['GET'])
+def generate_key_route():
+    key_dict = generate_key()
+    keymap = key_to_string(key_dict)
+    return render_template('index.html', encrypted="", keymap=keymap, plaintext="")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
